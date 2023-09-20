e97d2c6e4dd5377d17a28e3dfbc0a0d558dc2674
cyber-shah <shah.pranc@northeastern.edu>
2023-09-19 22:35:32 -0400

    good

---
+++ index.js
@@ -0,0 +1,11 @@
+// JavaScript is about behavior
+// HTML is about content
+console.log('wassup');
+let abs = "hey";
+abs = 9889272;
+
+
+
+// document.getElementById("myButton").addEventListener("click", function() {
+//     document.getElementById("demo").innerHTML = "Hello, GitHub Pages!";
+// });
