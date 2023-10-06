c0aac1e2bb27d985888751552546ea4d02910783
cyber-shah <pranchx@gmail.com>
2023-10-06 19:15:46 -0400

    login

--- django_property/website/views.py
+++ django_property/website/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.shortcuts import redirect
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html', {})
+    # check to see if logging in 
+    # so we see if this request method is a post?
+    # if method is post, it means they are submitting the form
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # authenticate user
+        user = authenticate(request, username=username, password=password)
+        # if user is not none, then user is authenticated
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have been logged in!'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Error logging in - please try again...'))
+            return redirect('home')
+    
+    # so if this function is not called via "POSTING" the form, then we just want to render the page
+    # and assume that the user is not logged in
+    else:
+        return render(request, 'base.html', {})
 
+# def login_user(request):
+#     pass
 
-def login_user(request):
-    pass
-
-def logout_user(request):
-    pass
+# def logout_user(request):
+#     pass
 
