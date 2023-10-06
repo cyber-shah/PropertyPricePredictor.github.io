652253002eb329c4a2cf8cd248aeffc73084d858
cyber-shah <pranchx@gmail.com>
2023-10-05 21:32:44 -0400

    features

--- myapp/insideapp/views.py
+++ myapp/insideapp/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    # this is dynamic data! 
-    context = {
-        'name' : 'Makkali Gosala',
-        'age' : 23,
-        'nationality' : 'Magadhi'
-    }
-    return render(request, 'index.html', context)
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Price Graph'
+    
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Indicator 1'
+    
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Indicator 2'
+    
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Indicator 3'
+
+    return render(request, 'index.html', {'Feature' : feature1,  })
 
 
 # so when someone visits this url, this function will be called
@@ -21,3 +31,5 @@ def counter(request):
     text = request.POST['text']
     amount_of_words = len(text.split())
     return render (request, 'counter.html', {'amount_of_words' : amount_of_words})
+
+
